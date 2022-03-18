8a74c79cba07a51f4b584a901b2c7fea0d32fac6
Armine Gasparyan <armin.gasparian@gmail.com>
2022-03-18 18:54:56 +0400

    coming soon pages

---
+++ Coming Soon 2/index.js
@@ -0,0 +1,21 @@
+
+let countDownDate = new Date("June 4, 2022 00:00:00").getTime();
+let myCount = setInterval(() => {
+  let now = new Date().getTime();
+  let distance = countDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById('days').innerHTML = days;
+  document.getElementById('hours').innerHTML = hours;
+  document.getElementById('minutes').innerHTML = minutes;
+  document.getElementById('seconds').innerHTML = seconds;
+
+  if(distance < 0) {
+    clearInterval(myCount);
+    document.getElementById('days').innerHTML = 'Congratulations';
+  }
+}, 1000);
